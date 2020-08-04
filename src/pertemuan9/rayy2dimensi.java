fca0c226890c66d4bf4c8f17590bc7efb2dae6bb
ASUS <ASUS@LAPTOP-0VGVO88P>
2020-08-04 09:29:36 +0700

    uas

---
+++ src/pertemuan9/rayy2dimensi.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author ASUS
+ */
+public class rayy2dimensi {
+    
+        public static void main(String[] args) {
+        String [][] Laptop ={ {"asus","6.000.000"},{"hp","5.000.000"},{"lenovo","7.000.000"},};
+        for (int x=0; x<Laptop.length;x++){
+            System.out.println("merk : "+Laptop [x] [0]);
+              System.out.println("merk : "+Laptop [x] [1]);
+              System.out.println("----------------------------");
+        }
+            
+        }
+        
+    }
+    
+    
+    
+
